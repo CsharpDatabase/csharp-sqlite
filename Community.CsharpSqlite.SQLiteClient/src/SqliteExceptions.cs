3b0ddf3cda99829cbd6dbcf265689ec6dac1340e
Noah <devnull@localhost>
2011-07-03 07:23:56 -0700

    Fixed Issue 121 -- Submittd by high...

--- Community.CsharpSqlite.SQLiteClient/src/SqliteExceptions.cs
+++ Community.CsharpSqlite.SQLiteClient/src/SqliteExceptions.cs
@@ -1,56 +1,79 @@
 using System;
 using System.Data;
+using System.Data.Common;
 
-namespace Community.CsharpSqlite.SQLiteClient 
+namespace Community.CsharpSqlite.SQLiteClient
 {
-	// This exception is raised whenever a statement cannot be compiled.
-	public class SqliteSyntaxException : ApplicationException
-	{
-		public SqliteSyntaxException() : this("An error occurred compiling the Sqlite command.")
-		{
-		}
-		
-		public SqliteSyntaxException(string message) : base(message)
-		{
-		}
+    //This is the base exception of all sqlite exceptions
+    public class SqliteException : DbException
+    {
+        public int SqliteErrorCode { get; protected set; }
 
-		public SqliteSyntaxException(string message, Exception cause) : base(message, cause)
-		{
-		}
-	}
+        public SqliteException(int errcode)
+            : this(errcode, string.Empty)
+        {
+        }
 
-	// This exception is raised whenever the execution
-	// of a statement fails.
-	public class SqliteExecutionException : ApplicationException
-	{
-		public SqliteExecutionException() : this("An error occurred executing the Sqlite command.")
-		{
-		}
-		
-		public SqliteExecutionException(string message) : base(message)
-		{
-		}
+        public SqliteException(int errcode, string message)
+            : base(message)
+        {
+            SqliteErrorCode = errcode;
+        }
 
-		public SqliteExecutionException(string message, Exception cause) : base(message, cause)
-		{
-		}
-	}
+        public SqliteException(string message)
+            : this(0, message)
+        {
+        }
+    }
 
-	// This exception is raised whenever Sqlite says it
-	// cannot run a command because something is busy.
-	public class SqliteBusyException : SqliteExecutionException
-	{
-		public SqliteBusyException() : this("The database is locked.")
-		{
-		}
-		
-		public SqliteBusyException(string message) : base(message)
-		{
-		}
+    // This exception is raised whenever a statement cannot be compiled.
+    public class SqliteSyntaxException : SqliteException
+    {
+        public SqliteSyntaxException(int errcode)
+            : base(errcode)
+        {
 
-		public SqliteBusyException(string message, Exception cause) : base(message, cause)
-		{
-		}
-	}
+        }
+        public SqliteSyntaxException(int errcode, string message)
+            : base(errcode, message)
+        {
+        }
+        public SqliteSyntaxException(string message)
+            : base(message)
+        {
+        }
+    }
+
+    // This exception is raised whenever the execution
+    // of a statement fails.
+    public class SqliteExecutionException : SqliteException
+    {
+        public SqliteExecutionException()
+            : base(0)
+        {
+        }
+        public SqliteExecutionException(int errcode)
+            : base(errcode)
+        {
+        }
+        public SqliteExecutionException(int errcode, string message)
+            : base(errcode, message)
+        {
+        }
+        public SqliteExecutionException(string message)
+            : base(message)
+        {
+        }
+    }
+
+    // This exception is raised whenever Sqlite says it
+    // cannot run a command because something is busy.
+    public class SqliteBusyException : SqliteException
+    {
+        public SqliteBusyException()
+            : base(0)
+        {
+        }
+    }
 
 }
